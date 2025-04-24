45449f02d1a5859029a544a8ae0fd1402ebf5b4f
sophiamcc1 <sophiamcc122@gmail.com>
2025-04-24 16:40:42 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,19 @@
+// Author: Sophia McClanahan
+// Date: 24 April 2025
+// License: Public Domain
+
+//Define Variables
+make = "Toyota";
+model = "Corolla";
+color = "white"; 
+year = 2024;
+
+//calculate
+age = 2025 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>"); 
+document.writeln("Year: " + year + "<br>"); 
+document.writeln("Age: " + age + "years<br>");
