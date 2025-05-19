f12364e4d4a7b3f83cd65a48b750117b86f547c5
sophiamcc1 <sophiamcc122@gmail.com>
2025-05-19 16:24:16 -0700

    lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,3 +7,23 @@
    Date: 19 May 2025
 */
 
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function(){
+
+// get value of input field
+const userName = $("#user-name").val();
+
+// now sort it
+userNameSorted = sortString(userName);
+
+// append a new div to output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+});
+
