34bcb276a0552e4aebdcb01edec33db9ffd646d7
sophiamcc1 <sophiamcc122@gmail.com>
2025-05-19 16:07:02 -0700

    lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,9 @@
+/*
+   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Sophia McClanahan
+   Date: 19 May 2025
+*/
+
