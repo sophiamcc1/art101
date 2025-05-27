17e1ced2b48e03c2f6ac85a94d84a698a4694ff6
sophiamcc1 <sophiamcc122@gmail.com>
2025-05-26 23:29:33 -0700

    lab13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,16 +7,34 @@
    Date: 26 May 2025
 */
 
-for (i = 1; i <= 200; i++) {
-  if (i % 15 == 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 == 0) {
-    console.log("Fizz");
-  } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
+$("action").click(function() {
+    // credit to W3Schools
+    var outputElement = document.getElementById('output');
+
+    var data = fizzBuzzBoom();
+
+    // credit to Wesbot
+    var html = '';
+    for (var i = 0; i < data.length; i++) {
+        html += '<p>' + data[i] + '</p>';
+    }
+    outputElement.innerHTML = html;
+});
+
+function fizzBuzzBoom () {
+    const logArray = [];
+    for (i = 1; i <= 200; i++) {
+      if (i % 15 == 0) {
+        logArray.push("FizzBuzz");
+      } else if (i % 3 == 0) {
+        logArray.push("Fizz");
+      } else if (i % 5 == 0) {
+        logArray.push("Buzz");
+      } else if (i % 7 == 0) {
+        logArray.push("Boom");
+      } else {
+        logArray.push(i);
+      }
+    }
+    return(logArray);
 }
-
-
