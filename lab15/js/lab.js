dd3fad8b0b14f7938884496f9a922b83ebca40ac
sophiamcc1 <sophiamcc122@gmail.com>
2025-06-02 18:24:27 -0700

    lab updates

---
+++ lab15/js/lab.js
@@ -0,0 +1,28 @@
+const ajaxObj = {
+  url: "https://yesno.wtf/api",
+  // data: {},
+  type: "GET",
+  dataType: "json"
+}
+
+$("#activate").click(function () {
+  console.log("click!");
+  
+  $.ajax(ajaxObj)
+  
+  .done(function(data) {
+    console.log("Success!");
+    console.log(data);
+    
+    let answer = data.answer;
+    let imgURL = data.image;
+    let imageHTML = "<img src = '" + imgURL + "'>";
+    
+    $("#output").html("<h1>" + answer);
+    $("#output").append(imageHTML); 
+  })
+  
+  .fail(function( xhr, status, errorThrown ) {
+    console.log(errorThrown + " Status:" + status);
+  });
+})
